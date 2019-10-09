a2e3a177858b73562b215bf4248709d8653c7c2a
Gregory Bonvalot <talock69@hotmail.com>
2019-10-09 17:35:46 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div>
+        <Navbar/>
       </div>
     );
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Navbar = () => {
+    return (
+    <nav>
+        <a href="http://www.simpsonspark.com/">Les Simpsons</a>
+    </nav>
+    );
+}
+
+export default Navbar;
