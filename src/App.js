9a23b81dc9bfa6a935729a1a7eb19394b93573b6
Gregory Bonvalot <talock69@hotmail.com>
2019-10-10 09:34:45 +0200

    quotecard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import Navbar from './component/Navbar';
-
+import QuoteCard from './component/QuoteCard';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar/>
+        <QuoteCard/>
       </div>
     );
   }
---
+++ src/component/QuoteCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './QuoteCard.css'
+
+const QuoteCard = () => {
+    return (
+    <div>
+        <img src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
+      alt="Nelson Muntz"></img>
+        <blockquote>Shoplifting is a victimless crime, like punching someone in the dark.</blockquote>
+        <cite>Nelson Muntz</cite>
+    </div>
+    );
+}
+
+
+export default QuoteCard;
